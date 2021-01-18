029e6f4eae0f232e677a6bbf05f00d06ecf5a32f
{rnantes} <{reid.nantes@gmail.com}>
2021-01-18 11:03:41 -0500

    add simple createSimpleDB

--- Sources/SomeDBLib/SomeDBLib.swift
+++ Sources/SomeDBLib/SomeDBLib.swift
@@ -6,4 +6,25 @@ public struct SomeDBLib {
     public func foo() -> String {
         return "bar"
     }
+
+    public func createSimpleDB(dbPath: String) throws {
+        let dbQueue = try DatabaseQueue(path: dbPath)
+
+        try dbQueue.write { db in
+            try db.execute(sql: """
+            CREATE TABLE player (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                score INT)
+            """)
+
+            try db.execute(
+                sql: "INSERT INTO player (name, score) VALUES (?, ?)",
+                arguments: ["Barbara", 1000])
+
+            try db.execute(
+                sql: "UPDATE player SET score = :score WHERE id = :id",
+                arguments: ["score": 1000, "id": 1])
+        }
+    }
 }
